1ca0b2870a3c0878240be1cecd0db681a40a9817
AthariCoded <athari.rashed@gmail.com>
2021-05-24 17:13:24 +0300

    Task Monday

---
+++ story.js
@@ -0,0 +1,5 @@
+let name = "Laila";
+let age = 25;
+let hoppy = "hoarding Books";
+console.log(`mr ${name}, in 33 years, you will be ${age +30} years old.`);
+console.log(`By then you'll have decades of ${hoppy} experience. `);
