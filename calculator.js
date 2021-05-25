e88b06b6c14b433bf24030e74819d8f86c705a54
AthariCoded <athari.rashed@gmail.com>
2021-05-25 15:47:06 +0300

    solved with result

--- calculator.js
+++ calculator.js
@@ -1,10 +1,30 @@
 
 let num1 = 5
 let num2 = 3
+let result;
 
-let operation = "+";
+let operation = "*";
 
-if (operation === "+")
-    console.log(`${num1} + ${num2}`);
+if (operation === "+"){
+    result = num1+num2;
+    console.log(`${num1} + ${num2} = ${result}`);
+}
+ else if (operation === "-"){
+    result = num1-num2; 
+    console.log(`${num1} - ${num2} = ${result}`);
+ }
+else if (operation === "*"){
+    result = num1*num2;
+    console.log(`${num1} * ${num2} = ${result}`);
+}    
+else if (operation === "/"){
+    result = num1/num2;
+    console.log(`${num1} / ${num2} = ${result}`);
+}   
 
-    
+console.log(`Please enter the first number: ${num1}`);
+console.log(`Please enter the second number: ${num2}`);
+console.log(`Please choose the operation (+, -, /, *): ${operation}`);
+console.log(`--------------`);
+console.log(`${num1} ${operation} ${num2} = ${result}`);
+
